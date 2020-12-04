26b1ffd5e20700b76726c2aac4a0490c1beee814
masayado <masayado@uc.cl>
2020-12-04 16:05:29 -0300

    funcion para cambiar color no funciona

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,6 +36,9 @@ const fetchData = async (id) => {
 
 const renderizeCard = (pokemon) => {
     console.log(pokemon)
+    //const cardHeader = document.querySelector('.card__header');
+    //const pokeTypes = document.querySelector('.card_body_types-1');
+    //const dataTitles = document.querySelector('.card_data_title');
     const container = document.querySelector('.container');
     const template = document.querySelector('#template-card').content;
     const clone = template.cloneNode(true);
@@ -56,3 +59,80 @@ const renderizeCard = (pokemon) => {
     fragment.appendChild(clone);
     container.appendChild(fragment);
 }
+
+(function (window, document, undefined) {
+
+    window.onload = init;
+
+    function init() {
+
+        const elem = document.getElementById('cardHeader');
+        //const elem = document.getElementsByClassName('prueba');
+        let pokeType = type
+        
+        
+        switch (pokeType) {
+            case "normal":
+                elem.style.backgroundColor = '#c7ccce'
+                break;
+            case "poison":
+                elem.style.backgroundColor = '#b090c1'
+                break;
+            case "psychic":
+                elem.style.backgroundColor = '#db75ae'
+                break;
+            case "grass":
+                elem.style.backgroundColor = '#8bbe8a'
+                break;
+            case "ground":
+                elem.style.backgroundColor = '#c4943f'
+                break;
+            case "ice":
+                elem.style.backgroundColor = '#62cdc1'
+                break;
+            case "fire":
+                elem.style.backgroundColor = '#ffa756'
+                break;
+            case "rock":
+                elem.style.backgroundColor = '#bca584'
+                break;
+            case "dragon":
+                elem.style.backgroundColor = '#9a9dcb'
+                break;
+            case "water":
+                elem.style.backgroundColor = '#58abf6'
+                break;
+            case "bug":
+                elem.style.backgroundColor = '#8cb331'
+                break;
+            case "dark":
+                elem.style.backgroundColor = '#5a595d'
+                break;
+            case "fighting":
+                elem.style.backgroundColor = '#eb4971'
+                break;
+            case "fire":
+                elem.style.backgroundColor = '#ffa756'
+                break;
+            case "ghost":
+                elem.style.backgroundColor = '#636298'
+                break;
+            case "steel":
+                elem.style.backgroundColor = '#447f9c'
+                break;
+            case "flying":
+                elem.style.backgroundColor = '#b5bbd3'
+                break;
+            case "electric":
+                elem.style.backgroundColor = '#f2cb55'
+                break;
+            case "fairy":
+                elem.style.backgroundColor = '#eab8cf'
+                break;
+            default:
+                elem.style.backgroundColor = '#ebedf5'
+                break;
+        }
+    }
+
+})(window, document, undefined);
