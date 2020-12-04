bf3bc8afaf13e3877d78127679d42caa5794a5fc
masayado <masayado@uc.cl>
2020-12-04 04:50:30 -0300

    renderizando datos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,59 @@
 //DOM Objects
-const pokeName = document.querySelector('.poke-name');
+const pokeName = document.querySelector('.card__header__details-name');
 const pokeId = document.querySelector('.poke-id');
 const pokeType = document.querySelector('.poke-type');
 
 console.log(pokeName, pokeId, pokeType);
 
-//Generando un numero random
+document.addEventListener('DOMContentLoaded', () =>{
+    const random = (getRandomInt(1, 151));
+    fetchData(random);
+})
+
+//Generando un numero random cada vez que refrescamos la pagina
 const getRandomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-console.log(getRandomInt(1, 151));
-
-document.addEventListener('DOMContentLoaded', () =>{
-    fetchData();
-})
-
-const fetchData = async () => {
+const fetchData = async (id) => {
     try{
-        const res = await fetch('https://pokeapi.co/api/v2/pokemon/1')
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
         const data = await res.json()
-        console.log(data)
+
+        console.log(data);
+
+        const pokemon = {
+            img: data.sprites.other.dream_world.front_default,
+            name: data.name,
+            id: data.id,
+            height: data.height,
+            weight: data.weight,
+            species:data.species.name,
+            attack: data.stats[1].base_stat,
+            exp: data.base_experience,
+            defense: data.stats[3].base_stat,
+        };
+        renderizeCard(pokemon)
     } catch(error){
         console.log(error)
     }
+}
+
+const renderizeCard = (pokemon) => {
+    console.log(pokemon)
+    const container = document.querySelector('.container');
+    const template = document.querySelector('#template-card').content;
+    const clone = template.cloneNode(true);
+    const fragment = document.createDocumentFragment();
+
+    clone.querySelector('.card__header__details-name').innerHTML = `${pokemon.name}`;
+    clone.querySelector('.poke-id').innerHTML = '#' + `${pokemon.id}`;
+    clone.querySelector('.card__body__img').setAttribute('src', pokemon.img);
+    clone.querySelectorAll('.card-footer__stats p')[0].textContent = pokemon.attack;
+    clone.querySelectorAll('.card-footer__stats p')[2].textContent = pokemon.exp;
+    clone.querySelectorAll('.card-footer__stats p')[4].textContent = pokemon.defense;
+
+
+    fragment.appendChild(clone)
+    container.appendChild(fragment)
 }
